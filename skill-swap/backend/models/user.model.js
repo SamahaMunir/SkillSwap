7c9e8322bd6d7c86c5485754e49763964404d82d
SamahaMunir <119500740+SamahaMunir@users.noreply.github.com>
2025-05-16 20:48:43 +0500

    update user.model.js

---
+++ skill-swap/backend/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  skills: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Skill' }]
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare password
+userSchema.methods.matchPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
+
